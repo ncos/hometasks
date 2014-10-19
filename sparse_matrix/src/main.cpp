ae84bb6bb7a0abbe426dbeff6a585ebbf73bb87a
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2014-10-19 13:57:03 +0400

    finished and tested. seq_gen is slow

--- sparse_matrix/src/main.cpp
+++ sparse_matrix/src/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <iomanip>
+#include <cstring>
 #include <eigen3/Eigen/Dense>
 #include <vector>
 #include <cmath>
 #include "mlib.h"
 
 using Eigen::MatrixXd;
+
+
+static inline void loadbar(std::string str_, unsigned int x, unsigned int n, unsigned int w = 50) {
+    if ( (x != n) && (x % (n/100+1) != 0) ) return;
+
+    float ratio  =  x/(float)n;
+    int   c      =  ratio * w;
+
+    std::cout << str_;
+    std::cout << std::setw(3) << (int)(ratio*100) << "% [";
+    for (int x=0; x<c; x++) std::cout << "=";
+    for (int x=c; x<w; x++) std::cout << " ";
+    std::cout << "]\r" << std::flush;
+};
 
 
 
@@ -19,54 +35,54 @@ void seq_gen(uint64 depth, std::vector<uint64> &seq)
     seq.push_back(2);
 
     for (uint64 n = 2; n <= depth; ++n) {
+        loadbar("Generating sequence: ", n, depth);
         for (int i = seq.size() - 1; i >= 0; i--) {
             std::vector<uint64>  to_insert;
             to_insert.push_back ((1 << n) + 1 - seq.at(i));
             seq.insert(seq.begin() + i + 1, to_insert[0]);
         }
     }
+    std::cout << "\n";
 };
 
 
 double get_thau(double L, double l, uint64 j, uint64 N)
 {
-    //return 1.0/((L + l)/2.0 + ((L - l)/2.0) * cos(M_PI*(2*j-1)/(2*N)));
-    return 2.0/(L + l);
+    return -1.0/((L + l)/2.0 + ((L - l)/2.0) * cos(M_PI*(2*j-1)/(2*N)));
 };
 
 
 int main()
 {
-    uint64 depth = 15;
+    uint64 depth = 18;
     matVec mat_vec = mat_vec_from_file("matrix.txt");
-    std::cout << "Input matrix:\n" << mat_vec.mat << "\n\n";
-    std::cout << "Right side:\n"   << mat_vec.vec << "\n\n";
     Eigen::VectorXd eig_vec = vec_from_file ("eigenvalues.txt");
-    std::cout << "Matrix eigenvalues:\n" << eig_vec << "\n\n";
 
-    std::vector<uint64> seq;
-    seq_gen(depth, seq);
-
+    std::cout << "Input matrix:\n" << mat_vec.mat << "\n\n";
+    std::cout << "Right side:\n"   << mat_vec.vec.transpose() << "\n\n";
+    std::cout << "Matrix eigenvalues:\n" << eig_vec.transpose() << "\n\n";
     std::cout << "Sequence (" << (1 << depth) << " numbers):\n";
-    for (int i = 0; i < seq.size(); ++i) {
-        std::cout << seq.at(i) << " ";
-    }
-    std::cout << std::endl;
-
     double L = eig_vec.maxCoeff();
     double l = eig_vec.minCoeff();
     std::cout << "minimum eigenvalue: " << l << "; maximum: " << L << "\n";
 
+    std::vector<uint64> seq;
+    seq_gen(depth, seq);
+
     Eigen::VectorXd u = Eigen::VectorXd::Zero(mat_vec.mat.rows());
-    std::cout << "Starting from u = :\n" << u << "\n";
+
     for (int i = 0; i < seq.size(); ++i) {
         int j = seq.at(i);
         double thau = get_thau(L, l, j, (1 << depth));
-        std::cout << "thau(" << j << ") = " << thau << "\n";
         u = u + thau * (mat_vec.mat * u - mat_vec.vec);
-        std::cout << "u(" << j << ") = :\n" << u << "\n";
+        loadbar("Calculating: ", i + 1, seq.size());
     }
 
-    std::cout << "The result is:\n" << u << "\n";
+    std::cout << "\n\n";
+    std::cout << "The result is:\n[";
+    for (int i = 0; i < u.rows() - 1; ++i) {
+        std::cout << u[i] << ", ";
+    }
+    std::cout << u[u.rows() - 1] << "]\n";
     return 0;
 };
--- sparse_matrix/src/matgen.cpp
+++ sparse_matrix/src/matgen.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
@@ -56,11 +57,43 @@ void mat_vec_to_file (Eigen::MatrixXd mat, Eigen::VectorXd vec, std::string fnam
     }
 
     else {
-        std::cerr << "Unable to open file '" << fname << "'\n";
+        std::cerr << "Unable to open file '" << fname << "\n";
     }
 };
 
+Eigen::MatrixXd rotation_matrix(int64 size, int64 ax1, int64 ax2, double angle) {
+    Eigen::MatrixXd mat(size, size);
+    mat.setIdentity();
+    if (ax1 == ax2) {
+        std::cerr << "ax1 == ax2 in rotation_matrix call!\n";
+        return mat;
+    }
+    if (ax1 >= size || ax2 >= size) {
+        std::cerr << "ax1 or ax2 > size in rotation_matrix call!\n";
+        return mat;
+    }
+    mat(ax1, ax1) =   cos(angle);
+    mat(ax2, ax2) =   cos(angle);
+    mat(ax1, ax2) = - sin(angle);
+    mat(ax2, ax1) =   sin(angle);
 
+
+    return mat;
+};
+
+Eigen::MatrixXd get_random_rotation(int64 size) {
+    Eigen::MatrixXd mat(size, size);
+    mat.setIdentity();
+
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = i + 1; j < size; ++j) {
+            double angle = (rand() % int(2 * M_PI * 100)) / 100.0 - M_PI;
+            mat *= rotation_matrix(size, i, j, angle);
+        }
+    }
+
+    return mat;
+};
 
 
 Eigen::MatrixXd generate_matrix(int64 size) {
@@ -85,19 +118,36 @@ matEig generate_matrix_eigenvalues(int64 size) {
     mat_eig.mat = Eigen::MatrixXd (size, size);
     mat_eig.eig = Eigen::VectorXd (size);
 
+    std::srand(std::time(0)); // use current time as seed for random generator
     for (uint64 i = 0; i < mat_eig.mat.rows(); ++i) {
         mat_eig.mat(i, i) = double(std::rand()) / 100000000.0;
         mat_eig.eig(i) = mat_eig.mat(i, i);
     }
 
+    Eigen::MatrixXd rot = get_random_rotation(size);
+    mat_eig.mat = rot * mat_eig.mat * rot.transpose();
+
     return mat_eig;
 };
 
 
+void print_mat_wolfram(Eigen::MatrixXd mat) {
+    std::cout << "Matrix:\n";
+    std::cout << "{";
+    for(int i = 0; i < mat.rows(); ++i) {
+        std::cout << "{";
+        for(int j = 0; j < mat.cols(); ++j) {
+            std::cout << mat(i, j);
+            if (j != mat.rows() - 1) std::cout << ", ";
+        }
+        if (i != mat.rows() - 1) std::cout << "},\n";
+    }
+    std::cout << "}}\n";
+};
 
-int main()
-{
-    uint64 size = 2;
+
+int main() {
+    uint64 size = 10;
     matEig mat_eig = generate_matrix_eigenvalues(size);
     Eigen::VectorXd answer_vec (size);
     std::srand(std::time(0)); // use current time as seed for random generator
@@ -106,11 +156,15 @@ int main()
 
     Eigen::VectorXd d_vec = mat_eig.mat * answer_vec;
 
-    std::cout << mat_eig.mat << "\n";
-    std::cout << mat_eig.eig << "\n";
+    std::cout << "\n";
+    print_mat_wolfram(mat_eig.mat);
+    std::cout << "Eigen:\n[" << mat_eig.eig.transpose() << "]\n\n";
+    std::cout << "The right side vector:\n[" << d_vec.transpose() << "]\n\n";
+    std::cout << "The correct answers are:\n[" << answer_vec.transpose() << "]\n\n";
 
     mat_vec_to_file (mat_eig.mat, d_vec, "matrix.txt");
     vec_to_file (mat_eig.eig, "eigenvalues.txt");
-    vec_to_file (answer_vec, "precise_answers.txt");
+    vec_to_file (answer_vec, "correct_answers.txt");
+
     return 0;
 };
