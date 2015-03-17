9170f426f4a69e3ac926da6f8eac5767dfe4bd08
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2015-03-17 17:41:00 +0300

    this task works wel enough

--- CompMath_II_term/task_1.py
+++ CompMath_II_term/task_1.py
@@ -2,12 +2,6 @@
 # y'' - 2y'-y = -2xe^x, 0 <= x <= 1, y'(0)-y(0)=1, y(1)=e(2cosh(sqrt(2)+1)
 
 
-
-# cb0
-# acb
-# 0ac
-# a = [0, ...]; b = [..., 0]
-
 import numpy as np 
  
 ## Tri Diagonal Matrix Algorithm(a.k.a Thomas algorithm) solver
@@ -35,6 +29,7 @@ def TDMA(a, b, c, d):
 
 
 '''
+# Example:
 M = [[1,2,0,0],
      [5,3,2,0],
      [0,9,3,5],
@@ -53,6 +48,7 @@ N = 5
 a = 0.0
 b = 1.0
 h = (b-a)/float(N)
+ya = 1.0 
 
 X = [a+i*h for i in xrange(N+1)]
 
@@ -61,17 +57,32 @@ print ("N = "+str(N)+"; solving on ("+str(a)+", "+str(b)+"); h = "+str(h))
 print X
 
 def q(x):
+    return 1.0
 
 def p(x):
+    return x
 
 def f(x):
+    return 0.0
+
+b = [-2.0-h**2*q(X[i]) for i in range(1, N, 1)]
+c = [1.0+p(X[i])*h/2.0 for i in range(1, N-1, 1)]
+a = [0.0] + [1.0-p(X[i])*h/2.0 for i in range(2, N, 1)]
+f = [h**2*f(X[i]) for i in range(1, N, 1)]
+f[0] = f[0] - (1.0-p(X[1])*h/2.0)*ya
+
+a += [-1.0/h]
+b += [2.0+1.0/h]
+c += [1.0+p(X[N-1])*h/2.0] + [0.0]
+f += [0.0]
 
 
-b = [-2.0+h**2*q(X[i]) for i in xrange(N)]
-a = [0.0] + [1.0-p(X[i])*h/2.0 for i in range(1, N-1)]
-c = [1.0+p(X[i])*h/2.0 for i in range(1, N-1)] + [0.0]
-f = [h**2*f(] + []
 
+print b
+print a
+print c
+print f
 
-
+y = TDMA(a, b, c, f)
+print "y=", y 
 
