dd524d5e2ce8728cb947e7247230c57351178ac0
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2017-03-29 16:09:43 -0400

    [CMSC733]: P3 (7.2 & 7.3)

--- CMSC733/P3/Code/NonlinearPnP.m
+++ CMSC733/P3/Code/NonlinearPnP.m
@@ -1,13 +1,29 @@
-function [C,R] = NonlinearPnP(X, x, K, C, R)
+function [C, R] = NonlinearPnP(X, x, K, C, R)
 %% Inputs and Outputs
 % X and x: Nx3 and Nx2 matrices whose row represents correspondences 
 %                  between 3D and 2D points, respectively.
 % K: intrinsic parameter
 % C and R: for pose
 
-opts = optimoptions(@lsqnonlin, 'Algorithm', 'levenberg-marquardt', 'MaxIter', 1e3, 'Display', 'none');
 
-%% Your code goes here
+% x = [x, ones(size(x,1),1)];
+% X = [X, ones(size(X,1),1)];
 
+    opts = optimoptions(@lsqnonlin, 'Algorithm', 'levenberg-marquardt', 'MaxIter', 1e3, 'Display', 'none');
 
+    q0 = R2q(R);
+    qC0 = [q0; C];
+
+    qC = lsqnonlin(@Rerror_q, qC0, [], [], opts, X, x, K);
+    q = qC(1:4);
+    R = q2R(q);
+    C = qC(5:7);
 end
+
+
+function err = Rerror_q(qC, X, x, K)
+    P = K * q2R(qC(1:4)) * [eye(3), -qC(5:7)];
+    projection = bsxfun(@rdivide, P(1:2, :)*X', P(3,:)*X');
+    err = (x(:, 1:2)' - projection)';
+end
+
--- CMSC733/P3/Code/PnPRANSAC.m
+++ CMSC733/P3/Code/PnPRANSAC.m
@@ -1,4 +1,4 @@
-function [C,R,idx] = PnPRANSAC(X, x, K)
+function [C, R, idx] = PnPRANSAC(X, x, K, threshold, max_iter)
 %% Inputs
 % K : camera calibration matrix
 % X : 3D coordinates
@@ -8,5 +8,35 @@ function [C,R,idx] = PnPRANSAC(X, x, K)
 % R : Rotation matrix
 % idx : inlier index
 
-%% Your code goes here
+N = size(X, 1);
+R = [];
+C = [];
+if N < 6
+    return
+end
+X_aug = [X ones(N, 1)];
+n_best = 0;
+
+median_err = zeros(max_iter, 1);
+for i = 1 : max_iter
+    r_idx = ceil(rand(6,1)*N);
+    [C_temp, R_temp] = LinearPnP(X(r_idx,:),x(r_idx,:),K);
+
+    P = K*R_temp*[eye(3) -C_temp];
+    x_p = bsxfun(@rdivide,P(1:2,:)*X_aug',P(3,:)*X_aug')';
+    
+    err = sqrt(sum((x-x_p).^2,2));
+    mask = err < threshold;
+    median_err(i) = median(err);
+
+    num_in = sum(mask);
+    if n_best < num_in
+    	n_best = num_in;
+        C = C_temp;
+        R = R_temp;
+        inliers = mask(:);
+    end
+end
+
+[C, R] = LinearPnP(X(inliers,:), x(inliers,:), K);
 end
